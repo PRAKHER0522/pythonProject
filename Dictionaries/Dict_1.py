195771b258ed6d5bb503a91dffcc07fc8542d42a
PRAKHER0522 <prakher.bhargava@gmail.com>
2021-09-11 23:24:40 +0530

    Testing Git

---
+++ Dictionaries/Dict_1.py
@@ -0,0 +1,17 @@
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index('germany')
+
+# Use ind_ger to print out capital of Germany
+print('capital of germany is: ' + capitals[ind_ger])
+
+# From string in countries and capitals, create dictionary europe
+
+europe = {'spain':'madrid','france':'paris','germany':'berlin','norway':'oslo'}
+
+#print europe
+
+print(europe)
---
+++ calculator.py
@@ -0,0 +1,10 @@
+book = {
+    'title': 'The Giver',
+    'author': 'Lois Lowry',
+    'rating': 4.13
+}
+
+book.append(format: paperback)
+
+
+book['format']
