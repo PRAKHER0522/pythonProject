806250fe0e2c95cccd316437f56befe7c2ae7999
PRAKHER0522 <prakher.bhargava@gmail.com>
2021-09-13 17:13:04 +0530

    Commenting numpy library information

--- Numpy/Numpy_function.py
+++ Numpy/Numpy_function.py
@@ -1,7 +1,7 @@
 import numpy as np
 #creating numpy array from list.
 a = np.arange(15).reshape(3,5)
-b = np.array([[0.0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19]])
+b = np.array([[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19]],dtype=complex)
 print(a)
 print(b)
 #calculating dimension or axis of numpy array
